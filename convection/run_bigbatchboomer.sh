af7815d24af0030f768d51d3eecaa3b6bb6dd66a
Brandon <officialbrandonrogers422@gmail.com>
2023-08-13 01:21:22 -0500

    MORE SAFETY FOR PETES SAKE

--- convection/run_bigbatchboomer.sh
+++ convection/run_bigbatchboomer.sh
@@ -90,6 +90,49 @@
 
 
 
+## Lx=32 
+
+# Lx=32 Pr=1
+bash run.sh 8 4e3 1 200 32 
+bash run.sh 8 1e3 1 200 32
+bash run.sh 8 2e3 1 200 32
+bash run.sh 8 6e3 1 200 32
+
+bash run.sh 8 1e4 1 200 32
+bash run.sh 8 2e4 1 200 32
+bash run.sh 8 4e4 1 200 32
+bash run.sh 8 6e4 1 200 32
+
+bash run.sh 8 1e5 1 200 32
+bash run.sh 8 2e5 1 200 32
+bash run.sh 8 4e5 1 200 32
+bash run.sh 8 6e5 1 200 32
+
+bash run.sh 8 1e6 1 200 32
+bash run.sh 8 2e6 1 200 32
+bash run.sh 8 4e6 1 200 32
+bash run.sh 8 6e6 1 200 32
+
+# Lx=32 Pr=10
+bash run.sh 8 4e3 10 200 32 
+bash run.sh 8 1e3 10 200 32
+bash run.sh 8 2e3 10 200 32
+bash run.sh 8 6e3 10 200 32
+
+bash run.sh 8 1e4 10 200 32
+bash run.sh 8 2e4 10 200 32
+bash run.sh 8 4e4 10 200 32
+bash run.sh 8 6e4 10 200 32
+
+bash run.sh 8 1e5 10 200 32
+bash run.sh 8 2e5 10 200 32
+bash run.sh 8 4e5 10 200 32
+bash run.sh 8 6e5 10 200 32
+
+bash run.sh 8 1e6 10 200 32
+bash run.sh 8 2e6 10 200 32
+bash run.sh 8 4e6 10 200 32
+bash run.sh 8 6e6 10 200 32
 
 
 
